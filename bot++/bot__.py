c3ff11976748142b67d7444cb334db2e50551b92
siexp <mts@kyiv.ciklum.net>
2017-01-24 17:14:02 +0200

    Refactoring
    Moved upvoting to separate function

--- bot++/bot__.py
+++ bot++/bot__.py
@@ -1,33 +1,53 @@
+import argparse
 import requests
-import json
-import sys
+import asyncio
 
-def main( args ) :
-    if len( args ) != 3 :
-        return
+uid = None
+sid = None
+headers = None
 
-    auth = { "username": args[ 1 ], "password": args[ 2 ] }
+def upvote( uri, userId ) :
+    if ( userId != uid ) :
+        resp = requests.post( uri, data = { "vote": 1 }, headers=headers )
+        print( resp.text )
+
+def main( user, password, subdomain ) : 
+    # login
+    auth = { "username": user, "password": password }
     resp = requests.post( "https://dirty.ru/api/auth/login/", auth )
-
+    global uid
     uid = resp.json()[ 'uid' ]
+    global sid
     sid = resp.json()[ 'sid' ]
-
-    resp = requests.get( "https://dirty.ru/api/domains/karmageddon/posts/" )
-
-    posts = resp.json()[ 'posts' ]
-
+    global headers
     headers = { "X-Futuware-UID": uid, "X-Futuware-SID": sid }
-    for post in posts :
-        if ( str( post['user']['id'] ) != uid ) :
-            resp = requests.post( "https://dirty.ru/api/posts/" + str( post[ 'id' ] ) + "/vote/", data = { "vote": 1 }, headers=headers )
-            print( resp.text )
 
-        resp = requests.get( "https://dirty.ru/api/posts/1289082/comments/" )
+    # get list of all posts of subdomain
+    resp = requests.get( "https://dirty.ru/api/domains/" + subdomain + "/posts/" )
+    posts = resp.json()[ 'posts' ]
+    
+    for post in posts :        
+        # upvote each post that not belongs to you
+        postId = str( post[ 'id' ] )
+        postUserId = str( post['user']['id'] )
+        uri = "https://dirty.ru/api/posts/" + postId + "/vote/"
+        upvote( uri, postUserId )            
+
+        resp = requests.get( "https://dirty.ru/api/posts/" + postId + "/comments/" )
         comments = resp.json()[ 'comments' ]
+        # upvote each comment in the post
         for comment in comments :
-            if ( str( comment['user']['id'] ) != uid ) :
-                resp = requests.post( "https://dirty.ru/api/comments/" + str( comment[ 'id' ] ) + "/vote/", data = { "vote": 1 }, headers=headers )
-                print( resp.text )
+            commentId = str( comment[ 'id' ] )
+            commentUserId = str( comment['user']['id'] )
+            uri = "https://dirty.ru/api/comments/" + commentId + "/vote/"
+            upvote( uri, commentUserId )            
 
 if __name__ == "__main__":
-    main( sys.argv )
+    parser = argparse.ArgumentParser( description='PyScrip that allows to upvote each post and each comment in subdomain' )
+    
+    parser.add_argument( 'user', help = 'your username' )
+    parser.add_argument( 'password', help = 'your password' )
+    parser.add_argument( 'subdomain', help = 'subdomain to process' )
+
+    args = parser.parse_args()
+    main( args.user, args.password, args.subdomain )
