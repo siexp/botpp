6cc5e1c4cf3c4df8adee55ae6836c9385becee2d
siexp <elioni17@gmail.com>
2017-07-20 14:49:20 +0300

    Added cache
    From now script can store ids of post\comments that were voted and don't
    vote them again.
    Also, it can iterate over pages of given subdomain.

--- bot++/bot__.py
+++ bot++/bot__.py
@@ -1,19 +1,26 @@
 import argparse
+import json
 import requests
+import os
 
+# globals that probably will not change 
+# until script done
 uid = None
 sid = None
 headers = None
 
-def upvote( uri, userId ) :
+postPerPage = 20
+
+def vote( uri, userId, value ) :
     if ( userId != uid ) :
-        resp = requests.post( uri, data = { "vote": 1 }, headers=headers )
-        print( resp.text )
+        resp = requests.post( uri, data = { "vote": value }, headers=headers ).json()
+        print( resp )
 
 def main( user, password, subdomain ) : 
     # login
     auth = { "username": user, "password": password }
     resp = requests.post( "https://dirty.ru/api/auth/login/", auth )
+
     global uid
     uid = resp.json()[ 'uid' ]
     global sid
@@ -22,28 +29,60 @@ def main( user, password, subdomain ) :
     headers = { "X-Futuware-UID": uid, "X-Futuware-SID": sid }
 
     # get list of all posts of subdomain
-    resp = requests.get( "https://dirty.ru/api/domains/" + subdomain + "/posts/" )
-    posts = resp.json()[ 'posts' ]
-    
-    for post in posts :        
-        # upvote each post that not belongs to you
-        postId = str( post[ 'id' ] )
-        postUserId = str( post['user']['id'] )
-        uri = "https://dirty.ru/api/posts/" + postId + "/vote/"
-        upvote( uri, postUserId )            
+    pageId = 1
+    while True :
+        print( "Processing page #" + str( pageId ) + " of subdomain <" + subdomain + ">" )
+        resp = requests.get( "https://dirty.ru/api/domains/" + subdomain + "/posts/?page=" + str( pageId ) + ";per_page=" + str( postPerPage ) + ";sorting=date_created" )
+        posts = resp.json()[ 'posts' ]
 
-        resp = requests.get( "https://dirty.ru/api/posts/" + postId + "/comments/" )
-        comments = resp.json()[ 'comments' ]
-        # upvote each comment in the post
-        for comment in comments :
-            commentId = str( comment[ 'id' ] )
-            commentUserId = str( comment['user']['id'] )
-            uri = "https://dirty.ru/api/comments/" + commentId + "/vote/"
-            upvote( uri, commentUserId )            
+        voted = loadCache()
+        for post in posts :        
+            # upvote each post that not belongs to you
+            postId = str( post[ 'id' ] )
+            postUserId = str( post['user']['id'] )
+            uri = "https://dirty.ru/api/posts/" + postId + "/vote/"
+            if postId not in voted :
+                vote( uri, postUserId, 1 )
+                voted[ postId ] = json.loads('[]')                
+            else :
+                print( 'Already voted <' + postId + '>')
+
+            resp = requests.get( "https://dirty.ru/api/posts/" + postId + "/comments/" )
+            comments = resp.json()[ 'comments' ]
+            # upvote each comment in the post
+            for comment in comments :
+                commentId = str( comment[ 'id' ] )
+                commentUserId = str( comment['user']['id'] )
+                uri = "https://dirty.ru/api/comments/" + commentId + "/vote/"
+
+                if commentId not in voted[ postId ] :
+                    vote( uri, commentUserId, 1 )
+                    voted[ postId ].append( commentId )
+                else :
+                    print( 'Already voted <' + postId + ':' + commentId + '>' )
+
+        saveCache( voted )
+        if len( posts ) != postPerPage :
+            break
+        
+        pageId += 1       
+
+def loadCache() :
+    data = json.loads('{}')
+    if os.path.exists( 'cache.json' ) :
+        with open('cache.json') as cache :
+            data = json.load( cache )
+
+    return data
+
+def saveCache( cache ) :
+    with open('cache.json', 'w') as outfile:
+        json.dump( cache, outfile)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser( description='PyScrip that allows to upvote each post and each comment in subdomain' )
-    
+
     parser.add_argument( 'user', help = 'your username' )
     parser.add_argument( 'password', help = 'your password' )
     parser.add_argument( 'subdomain', help = 'subdomain to process' )
